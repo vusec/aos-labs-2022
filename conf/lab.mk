a0ae83cfeb509ae55dfe4da7039aba234d8057b5
Manuel Wiesinger <m@mmap.at>
2022-09-09 15:43:07 +0200

    lab2: On The Same Page

--- conf/lab.mk
+++ conf/lab.mk
@@ -1,6 +1,5 @@
-LAB=1
-PACKAGEDATE=Mon 02 Sep 2019 01:25:38 PM CEST
-PATCH_PREFIX=vu-aos-2019
+LAB=2
+#PACKAGEDATE=Mon 02 Sep 2019 01:25:38 PM CEST
+PATCH_PREFIX=vu-aos-$(shell date +%Y)
 TAG_LAB_SOLN=handin-lab${LAB}
 TAG_LAB_BONUS=handin-lab${LAB}-bonus
-
